8177876ad1d3e56f6250422899b71d82eb6fefd4
Ivan Lavriv <ivan.lavriv@perfectial.com>
2020-03-11 14:01:27 +0200

    Initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = 'ilavriv'
+SITENAME = 'Ivan Lavriv'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Helsinki'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = (('GitHub', 'https://github.com/lavriv92'),
+          ('Twitter', 'https://twitter.com/ilavriv'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
